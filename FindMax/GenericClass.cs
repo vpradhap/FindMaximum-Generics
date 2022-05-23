6bca9b0e831296dfb12729572ff68cc1291d244e
vpradhap <pradhapvenkat@gmail.com>
2022-05-23 19:19:26 +0530

    Refactored : Find maximum using generic class

---
+++ FindMax/GenericClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum_Generics.FindMax
+{
+    public class GenericClass<T> where T : IComparable
+    {
+        public T first , second , third ;
+        public GenericClass(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+
+        public static T Maximum(T first, T second, T third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                return first;
+            }
+        }
+        public T TestMax()
+        {
+            T max = GenericClass<T>.Maximum(this.first, this.second, this.third);
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ while (flag == 0)
     Console.WriteLine("\nList of choices");
     Console.WriteLine("1 - Find maximum using non-generic method");
     Console.WriteLine("2 - Find maximum using generic method");
+    Console.WriteLine("3 - Find maximum using generic class");
+
     Console.Write("\nYour Pick : ");
     int pick = Convert.ToInt32(Console.ReadLine());
     switch (pick)
@@ -18,7 +20,12 @@ while (flag == 0)
         case 2:
             Console.WriteLine("\nInteger Maximum Number using generic : " + Generic.Maximum(4, 16, 23));
             Console.WriteLine("Float Maximum Number using generic : " + Generic.Maximum(11.8, 4.3, 10.1));
-            Console.WriteLine("String Maximum Number using generic : " + Generic.Maximum("Appe", "Peach", "Banana"));
+            Console.WriteLine("String Maximum Number using generic : " + Generic.Maximum("Apple", "Peach", "Banana"));
+            break;
+        case 3:
+            Console.WriteLine("\nInteger Maximum Number using generic class : " + GenericClass<int>.Maximum(11, 4, 10));
+            Console.WriteLine("Float Maximum Number using generic class : " + GenericClass<double>.Maximum(1.3, 13.6, 21.7));
+            Console.WriteLine("string Maximum Number using generic class : " + GenericClass<string>.Maximum("apple", "Apple", "BANANA"));
             break;
         default:
             flag = 1;
