12026353e49c56b79fb7b60b52f8e6fa84f23552
vpradhap <pradhapvenkat@gmail.com>
2022-05-23 08:27:46 +0530

    Added : Find maximum between three integer numbers

---
+++ FindMax/FindMax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum_Generics.FindMax
+{
+    public class FindMax
+    {
+        public static int FindMaxInt(int first ,int second , int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using FindMaximum_Generics.FindMax;
+
+Console.WriteLine("Maximum Number is : "+FindMax.FindMaxInt(15,8,25));
