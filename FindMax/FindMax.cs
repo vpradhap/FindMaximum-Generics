22dad633f1c95de4ce81d25b22c055e2173c1eab
vpradhap <pradhapvenkat@gmail.com>
2022-05-23 08:39:41 +0530

    Added : Find maximum between three float numbers

--- FindMax/FindMax.cs
+++ FindMax/FindMax.cs
@@ -27,5 +27,24 @@ namespace FindMaximum_Generics.FindMax
                 return default;
             }
         }
+        public static double FindMaxDouble(double first, double second, double third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                return default;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿using FindMaximum_Generics.FindMax;
 
-Console.WriteLine("Maximum Number is : "+FindMax.FindMaxInt(15,8,25));
+Console.WriteLine("Integer Maximum Number : "+FindMax.FindMaxInt(15,8,25));
+Console.WriteLine("Float Maximum Number : " + FindMax.FindMaxDouble(15.2,25.6,32.1));
